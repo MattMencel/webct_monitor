8c87a1e1a789780c4ebb89fb976234a8df62af35
Matt Mencel <mr-mencel@wiu.edu>
2011-01-10 15:45:12 -0600

    edited HTML

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -8,12 +8,22 @@ describe PagesController do
       get 'daily'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'daily'
+      response.should have_tag("title", "WebCT Monitor | Daily")
+    end
   end
 
   describe "GET 'weekly'" do
     it "should be successful" do
       get 'weekly'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'weekly'
+      response.should have_tag("title", "WebCT Monitor | Weekly")
     end
   end
 
@@ -22,12 +32,22 @@ describe PagesController do
       get 'monthly'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'monthly'
+      response.should have_tag("title", "WebCT Monitor | Monthly")
+    end
   end
   
   describe "GET 'yearly'" do
     it "should be successful" do
       get 'yearly'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'yearly'
+      response.should have_tag("title", "WebCT Monitor | Yearly")
     end
   end
   
