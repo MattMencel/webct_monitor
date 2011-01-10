f2d67dc8162b62592a7798d1aa558a8be86ee5c5
Matt Mencel <mr-mencel@wiu.edu>
2011-01-10 15:54:35 -0600

    done with static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,14 +1,18 @@
 class PagesController < ApplicationController
   def daily
+    @title = "Daily"
   end
 
   def weekly
+    @title = "Weekly"
   end
 
   def monthly
+        @title = "Monthly"
   end
   
   def yearly
+        @title = "Yearly"
   end
 
 end
