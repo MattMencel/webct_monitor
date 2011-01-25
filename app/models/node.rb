fd316548cd0b83e054733bf18edc0c6abd955b32
Matt Mencel <mr-mencel@wiu.edu>
2011-01-25 15:31:46 -0600

    validation test

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,6 +1,6 @@
 class Node < ActiveRecord::Base
   has_many :sessions
   
-  validates_inclusion_of :type, :in => %w( application gateway ), :message => "{{value}} is not a valid type.  Must be [application] or [gateway]."
+  validates_inclusion_of :type, :in => %w( application gateway ), :message => "%s is not a valid type.  Must be [application] or [gateway]."
     
 end
