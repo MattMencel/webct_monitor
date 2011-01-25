b14f3ac93c84ec20716bf736a570eba6e8d3e592
Matt Mencel <mr-mencel@wiu.edu>
2011-01-25 15:15:52 -0600

    update

--- app/models/node.rb
+++ app/models/node.rb
@@ -1,3 +1,7 @@
 class Node < ActiveRecord::Base
   has_many :sessions
+  
+  validates_inclusion_of :type, :in => %w(application gateway),
+    :message => "{{value}} is not a valid type.  Must be application or gateway."
+    
 end
